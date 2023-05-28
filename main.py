696e382131e75ef1a67cc68a525d97ace047213a
IMOE001 <chani0533131761@gmail.com>
2023-05-28 17:03:58 +0300

    28/05

--- main.py
+++ main.py
@@ -35,5 +35,4 @@ print(result)  # יחזציר 3
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-   max_increasing_subsequence(arr)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pass
